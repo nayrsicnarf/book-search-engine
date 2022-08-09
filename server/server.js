2a6ca0c1f467c1b4ed7fbc474865754f4b93ad7e
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-08-09 09:46:20 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
-
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
